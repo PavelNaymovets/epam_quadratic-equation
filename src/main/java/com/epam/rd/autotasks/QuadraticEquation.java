067215617ed858bd34ad4df8791eb060d0052770
Pavel Naumovets <pavel.naumovets@mail.ru>
2021-05-26 22:44:34 +0300

    Answer on second task

--- src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
+++ src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
@@ -10,9 +10,18 @@ public class QuadraticEquation {
         double a = scanner.nextDouble();
         double b = scanner.nextDouble();
         double c = scanner.nextDouble();
-
-
-
+        double disc = b*b - 4*a*c;
+        if(disc<0){
+            System.out.println("no roots");
+        } else {
+            double x1 = (-b + Math.sqrt(disc))/(2*a);
+            double x2 = (-b - Math.sqrt(disc))/(2*a);
+            if(x1 == x2){
+                System.out.println(x1);
+            } else{
+                System.out.println(x1 + " " + x2 );
+            }
+        }
     }
 
 }
